145a38ccc760d99f3cd9f0b01bba9a89d2fcb191
Baromental <150538968+Baromental@users.noreply.github.com>
2024-09-25 14:13:49 +0000

    Deploying to gh-pages from @ Baromental/js-endless-practice@98754ddf39a5182a84854c2eebc74e77054608aa 🚀

---
+++ commonHelpers4.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const e=document.querySelector("input"),o=document.querySelector("span");e.addEventListener("input",u);function u(t){const n=t.currentTarget.value.trim();o.textContent=n||"Anonymous"}
+//# sourceMappingURL=commonHelpers4.js.map
---
+++ commonHelpers6.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const t=document.querySelector("body"),n=document.querySelector(".change-color"),e=document.querySelector(".color");n.addEventListener("click",r);function r(l){const o=c();t.style.backgroundColor=o,e.textContent=o}function c(){return`#${Math.floor(Math.random()*16777215).toString(16).padStart(6,0)}`}
+//# sourceMappingURL=commonHelpers6.js.map
