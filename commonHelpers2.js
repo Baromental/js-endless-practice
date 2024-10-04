e0566ceea43ea7a9e40151ed4ac579442d11e351
Baromental <150538968+Baromental@users.noreply.github.com>
2024-10-04 07:21:14 +0000

    Deploying to gh-pages from @ Baromental/js-endless-practice@d4ebf7a182b20632ecf554d442caed7d238b05c6 🚀

--- commonHelpers2.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const l=document.querySelectorAll(".item");console.log(`Number of categories: ${l.length}`);l.forEach(e=>{console.log(`Category:${e.firstElementChild.textContent}`),console.log(`Elements:${e.lastElementChild.children.length}`)});
-//# sourceMappingURL=commonHelpers2.js.map
--- commonHelpers5.js
+++
@@ -1,2 +0,0 @@
-import"./assets/modulepreload-polyfill-3cfb730f.js";/* empty css                      */const e=document.querySelector("form");e.addEventListener("submit",s);function s(r){r.preventDefault();const t=e.elements.email.value.trim(),o=e.elements.password.value.trim();!t||!o?alert("All form fields must be filled in"):(console.log({userEmail:t,userPassword:o}),e.reset())}
-//# sourceMappingURL=commonHelpers5.js.map
