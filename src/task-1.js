4116e7a1918552583400eba88897258e3d27316a
Maksym <suzdalcevmaksym@gmail.com>
2024-11-20 21:17:28 +0200

    new tasks

--- src/task-1.js
+++ src/task-1.js
@@ -1,10 +1,59 @@
+// const numbers = [1, 2, 3, 4, 5];
+// function calculateSum(numbers){
+//     let sum = 0;
+//     for (const key of numbers) {
+//         sum += key;
+//     }
+//     console.log(sum);
+    
+//     return sum;
+// }
 
-function slugify(title) {
-    return title.toLowerCase().split(' ').join('-');
+// calculateSum(numbers)
+
+// function isEven(num){
+//     return num % 2 === 0;
+
+// }
+// console.log(isEven(4));
+// console.log(isEven(7));
+
+const people = [
+    { name: 'Alice', age: 17 },
+    { name: 'Bob', age: 25 },
+    { name: 'Charlie', age: 19 },
+];
+
+
+function filterAdults(people){
+    let adults = [];
+
+    for (const person of people) {
+        if (person.age >= 18) {
+            adults.push(person.name);
+        }
+    }
+
+    return adults;
 }
 
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(filterAdults(people));
 
+
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 101);
+}
+console.log(generateRandomNumber());
+
+
+
+function startClock() {
+    setInterval(() => {
+        const now = new Date();
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+        console.log(`${hours}:${minutes}:${seconds}`);
+    }, 1000);
+}
+startClock();
